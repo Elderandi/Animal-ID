07203ab4d2f586540f2f067077f67d86532e1713
Elderandi <iva.anderson@hotmail.co.nz>
2017-05-30 10:00:44 -0600

    Test

--- angular.js
+++ angular.js
@@ -1,9 +1,27 @@
-angular.module('animalid', [])
-.config(function(){
+angular.module('animalid', ["ui.router"])
+.config(function($stateProvider, $urlRouterProvider){
+
+$stateProvider
+.state("home",{
+    url: "/",
+    templateUrl: "./home.html",
+    controller: "homeControl"
+})
+
+.state("frogexplore",{
+    url: "/amphibians",
+    templateUrl: "./views/frog-explore.html",
+    controller: "frogControl"
+})
+
+.state("bugexplore",{
+    url: "/arthropods",
+    templateUrl: "./views/bugs-explore.html",
+    controller: "bugsControl"
+})
 
 
 
-
-
+$urlRouterProvider.otherwise("/")
 
 });
--- controller/main-control.js
+++ controller/main-control.js
@@ -4,7 +4,7 @@ angular.module('animalid')
 
 
 
-$scope.test = "working";
+
 
 
   });
